c9610c7301715bd8a906fc18f32ff78a7197c2ba
Joshua <j2paz@ucsd.edu>
2019-02-07 09:09:40 -0800

    Changes

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,7 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
+exports.addFriend = function(request, response) { 
+  <script src= "js/bootstrap.js"></script>
+  <script src="js/hci-friends.js"></script>
+  <script src="js/jquery-1.11.0.js"></script>
  }
